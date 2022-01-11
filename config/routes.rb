230ae482e4ea0716d6dc130d65afcdbb43798d16
EC2 Default User <ec2-user@ip-172-31-16-63.ec2.internal>
2022-01-11 03:16:12 +0000

    [Add]3-12

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'lists/new'
   post 'lists' => 'lists#create'
-  get 'lists' => 'lists#index'
-  get 'lists/:id' => 'lists#show', as: 'list'
+  get 'lists' => 'lists#index', as: 'list'
+  get 'lists/:id' => 'lists#show', as: 'show_list'
   get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
   patch 'lists/:id' => 'lists#update', as: 'update_list'
   delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
